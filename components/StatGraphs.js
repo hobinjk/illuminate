713b7e74c801571341382842cbcb6c63251e1a3e
James Hobin <hobinjk@mit.edu>
2015-09-09 13:05:12 -0400

    Add graph transitioning; Remove graph circles; Modify auto-scaling

--- components/StatGraphs.js
+++ components/StatGraphs.js
@@ -12,33 +12,35 @@ class StatGraphs extends React.Component {
   }
 
   componentDidMount() {
-    this.createGraph();
+    let graphContainer = React.findDOMNode(this.refs.graphContainer);
+    let data = this.props.data;
+    this.createGraph(graphContainer, data);
+    this.updateGraph(graphContainer, data);
   }
 
-  createGraph() {
+  componentDidUpdate() {
+    let graphContainer = React.findDOMNode(this.refs.graphContainer);
     let data = this.props.data;
-    console.log(data);
+    this.updateGraph(graphContainer, data);
+  }
 
+  componentWillUnmount() {
+    let graphContainer = React.findDOMNode(this.refs.graphContainer);
+    this.destroyGraph(graphContainer);
+  }
+
+
+  createGraph(graphContainer, data) {
     if (!data || !data.length) {
       return;
     }
 
-    let graphContainer = React.findDOMNode(this.refs.graphContainer);
-    graphContainer.innerHTML = '';
     let margin = 8;
     let width = graphContainer.getBoundingClientRect().width - margin * 2;
     let height = graphContainer.getBoundingClientRect().height - margin * 2;
 
-    function yAccessor(d) {
-      return d.y;
-    }
-
-    function xAccessor(d) {
-      return d.x;
-    }
-
     let xScale = d3.scale.linear().range([0, width]);
-    xScale.domain([0, d3.max(data[0].data, xAccessor)]);
+    xScale.domain([0, d3.max(data[0].data, d => d.x)]);
 
 
     let svg = d3.select(graphContainer).append('svg')
@@ -46,89 +48,76 @@ class StatGraphs extends React.Component {
       .attr('height', height + 2 * margin)
       .append('g')
       .attr('transform', 'translate(' + margin + ',' + margin + ')');
+  }
+
+  updateGraph(graphContainer, data) {
+    if (!data || !data.length) {
+      console.log('No data for update');
+      return;
+    }
+
+    let margin = 8;
+    let width = graphContainer.getBoundingClientRect().width - margin * 2;
+    let height = graphContainer.getBoundingClientRect().height - margin * 2;
+
+    let xScale = d3.scale.linear().range([0, width]);
+    xScale.domain([0, d3.max(data[0].data, d => d.x)]);
 
     let colorScale = d3.scale.category20();
 
-    let tinyScale = d3.scale.linear().range([height, 0]);
-    let smallScale = d3.scale.linear().range([height, 0]);
-    let largeScale = d3.scale.linear().range([height, 0]);
+    // Scale data differently between 0-1, 1-500, and 500 upwards
+    let ranges = [0, 1, 500];
+    let rangedScales = ranges.map(range => d3.scale.linear().range([height, 0]));
+    let rangedData = ranges.map(range => []);
+    let rangedScaleMap = {};
 
-    let tinyData = [];
-    let smallData = [];
-    let largeData = [];
     data.forEach(series => {
-      if (series.name === 'Health' || series.name === 'Mana') {
-        largeData = largeData.concat(series.data);
-        return;
+      console.log('Processing ' + series.name);
+      let maxValue = d3.max(series.data, d => d.y);
+      for (let i = ranges.length - 1; i >= 0; i--) {
+        if (ranges[i] <= maxValue) {
+          rangedData[i] = rangedData[i].concat(series.data);
+          rangedScaleMap[series.name] = rangedScales[i];
+          break;
+        }
       }
-      if (series.name === 'Attacks Per Second') {
-        tinyData = tinyData.concat(series.data);
-        return;
-      }
-      smallData = smallData.concat(series.data);
     });
 
-    tinyScale.domain([d3.min(tinyData, yAccessor), d3.max(tinyData, yAccessor)]);
-    smallScale.domain([d3.min(smallData, yAccessor), d3.max(smallData, yAccessor)]);
-    largeScale.domain([d3.min(largeData, yAccessor), d3.max(largeData, yAccessor)]);
-
-
-    function getScale(name) {
-      if (name === 'Health' || name === 'Mana') {
-        return largeScale;
-      }
-      if (name === 'Attacks Per Second') {
-        return tinyScale;
-      }
-      return smallScale;
+    for (let i = 0; i < ranges.length; i++) {
+      rangedScales[i].domain([d3.min(rangedData[i], d => d.y), d3.max(rangedData[i], d => d.y)]);
     }
 
-    for (let series of data) {
-      console.log(series);
-      let graph = svg.append('g');
-
-      let yScale = getScale(series.name);
-
-      console.log(d3Tip);
-      let tip = d3Tip().attr('class', 'd3-tip')
-        .html(function(d) {
-          return series.name + ': ' + d.y;
-        });
-
-      graph.call(tip);
-
-      let circles = graph.selectAll('circle')
-        .data(series.data)
-
-      circles.enter()
-        .append('circle')
-        .attr('cx', function(d) { return xScale(d.x); })
-        .attr('cy', function(d) { return yScale(d.y); })
-        .attr('r', 8)
-        .attr('fill', colorScale(series.name))
-        .on('mouseover', tip.show)
-        .on('mouseout', tip.hide);
-
-      circles.exit()
-        .remove();
-
-      circles.transition()
-        .attr('cx', function(d) { return xScale(d.x); })
-        .attr('cy', function(d) { return yScale(d.y); })
-
-      let line = d3.svg.line()
-        .x(function(d) { return xScale(d.x);  })
-        .y(function(d) { return yScale(d.y);  });
-
-      let path = graph.append('path')
-        .datum(series.data)
-        .attr('d', line)
-        .attr('fill', 'none')
-        .attr('stroke', colorScale(series.name))
-        .attr('stroke-width', 2);
-
-      path.transition().attr('d', line);
+    function getScale(series) {
+      return rangedScaleMap[series.name];
     }
+
+    function makeSeriesLine(series) {
+      let yScale = getScale(series);
+
+      return d3.svg.line()
+                   .x(function(d) { return xScale(d.x); })
+                   .y(function(d) { return yScale(d.y); })
+                   (series.data);
+    }
+
+    let seriesLines = d3.select(graphContainer)
+      .select('svg')
+      .selectAll('path.series-line')
+      .data(data);
+
+    seriesLines.enter()
+      .append('path')
+      .attr('class', 'series-line')
+      .attr('fill', 'none')
+      .attr('stroke', function(series) {
+        return colorScale(series.name);
+      })
+      .attr('stroke-width', 2);
+
+    seriesLines.transition()
+      .attr('d', makeSeriesLine);
+
+    seriesLines.exit().remove();
 
     let legendContainer = d3.select(graphContainer)
       .append('div')
@@ -146,8 +135,7 @@ class StatGraphs extends React.Component {
           .style('color', function(d) { return colorScale(d.name); });
   }
 
-  componentDidUpdate() {
-    this.createGraph();
+  destroyGraph(graphContainer) {
   }
 
   render() {
